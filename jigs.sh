7ba77890d0ace10fda20941a1cc97fa4b2449b37
Jig-tech <57397335+Jig-tech@users.noreply.github.com>
2019-12-14 12:13:49 +0530

    Add files via upload

---
+++ jigs.sh
@@ -0,0 +1,3 @@
+figlet JIGS
+https://github.com/Jig-tech
+apt-get  install python python2 nano wget hydra git netcat curl perl php ruby tor nmap figlet w3m proot clang openssh vim emacs nodejs golang tsu neofetch sl cmatrix figlet toilet
